5924ca3fd0c2b67e9fc8e3fc665d333a0b6125c4
Wambui4 <loisewambzz@gmail.com>
2024-06-19 12:18:40 -0700

    added vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
-<script>
-
+<script setup>
+import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
-  <main> 
+  <v-app>
+  <Navbar/>
+  <v-main> 
     <router-view></router-view>
-  </main>
+  </v-main>
+  </v-app>
     </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,25 @@
 <template>
-    
+    <v-app-bar collapse :elevation="19">
+  <template v-slot:prepend>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  </template>
+
+
+
+  <v-app-bar-title>Application Bar</v-app-bar-title>
+  <v-btn to="/">Home</v-btn>
+
+  <v-btn to="/products">Products</v-btn>
+  <v-btn to="/aboutus">About Us</v-btn>
+  <v-btn to="/contactus">Contact Us</v-btn>
+  <v-btn to="/cart">Cart</v-btn>
+  <v-btn to="/services">Services Offered</v-btn>
+  
+
+
+
+
+
+
+</v-app-bar>
 </template>
